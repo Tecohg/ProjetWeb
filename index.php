178b26abf7e77a5080a89171043fe75a9da5d3e2
Tecohg <137790105+Tecohg@users.noreply.github.com>
2026-04-01 09:48:38 +0000

    test/chatbulle

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ProjetWeb</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>TEST</h1>
+    <p class="discussion">test</p>
+    <p class="message">test2<p></p>
 </body>
 </html>
