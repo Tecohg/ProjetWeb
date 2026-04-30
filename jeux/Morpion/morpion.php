4e35742db1731d9674b97d36b07f054e410636ad
Tecohg <137790105+Tecohg@users.noreply.github.com>
2026-04-30 12:48:26 +0000

    update morpion
    Co-authored-by: Iliden8426 <Iliden8426@users.noreply.github.com>

---
+++ jeux/Morpion/morpion.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+if (empty($_SESSION['login'])) {
+    header('Location: ../../identification/login.php');
+    exit;
+}
+$me = $_SESSION['login'];
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr"
+<head>
+    <meta charset="UTF-8">
+    <title>Jeu</title>
+    <link rel="stylesheet" href="jeux.css">
+</head>
+    <body>
+        <div id="interface">
+            <p id="status">TEST</p>
+        </div>
+
+<!-- tableau du morpion en utilisant les datasets, on stoque l'information dans la balise -->
+        <div id="tableau">
+            <?php for ($i=0; $i<9; $i++): ?>
+                <div class="case" data-index="<?= $i ?>"></div>
+            <?php endfor; ?>
+        </div>    
+
+        <!-- On envois $me au js puis on charge le script -->
+        <script>
+            const me = "<?= $me ?>"
+        </script>
+        <script src="morpion.js"></script>
+    </body>
+</html>
